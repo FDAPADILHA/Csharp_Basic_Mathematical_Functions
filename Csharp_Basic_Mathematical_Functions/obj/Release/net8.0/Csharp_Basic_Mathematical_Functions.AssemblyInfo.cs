0731306de5b1307240b5a88a3e77a01e49a1f1f3
Filipe do Amaral Padilha <filipe.padilha@maxima.local>
2024-10-09 08:12:16 -0300

    Including Bhaskara formula

--- Csharp_Basic_Mathematical_Functions/obj/Release/net8.0/Csharp_Basic_Mathematical_Functions.AssemblyInfo.cs
+++ Csharp_Basic_Mathematical_Functions/obj/Release/net8.0/Csharp_Basic_Mathematical_Functions.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp_Basic_Mathematical_Functions")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6545477759fe9ac8cfe8e65b2a16b4886e185b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp_Basic_Mathematical_Functions")]
